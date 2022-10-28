93d59e2e1f07c57fc88ce8f1c615aeeb53b89edf
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-10-28 20:53:50 -0300

    feat(aula155): create small, medium and large button and your tests

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,9 +1,12 @@
 import * as S from './styles';
 
-export const Button = () => {
+export type ButtonProps = {
+  children?: React.ReactNode;
+  size?: 'small' | 'medium' | 'large';
+};
+
+export const Button = ({ children, size = 'medium' }: ButtonProps) => {
   return (
-    <S.Wrapper>
-      <h1>Button</h1>
-    </S.Wrapper>
+    <S.Wrapper size={size}>{!!children && <span>{children}</span>}</S.Wrapper>
   );
 };
