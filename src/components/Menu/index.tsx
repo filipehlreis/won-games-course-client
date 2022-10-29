c18418cb78ff0b07f8c606830a6ea5d2f50ac5ca
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-10-29 17:35:34 -0300

    feat(aula164): create logic for user logged in and logged out

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -8,7 +8,11 @@ import { Logo } from 'components/Logo';
 import * as S from './styles';
 import { Button } from 'components/Button';
 
-export const Menu = () => {
+export type MenuProps = {
+  username?: string;
+};
+
+export const Menu = ({ username }: MenuProps) => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
@@ -33,17 +37,26 @@ export const Menu = () => {
         <S.MenuNav>
           <S.MenuLink href="#">Home</S.MenuLink>
           <S.MenuLink href="#">Explore</S.MenuLink>
+
+          {!!username && (
+            <>
+              <S.MenuLink href="#">My account</S.MenuLink>
+              <S.MenuLink href="#">Wishlist</S.MenuLink>
+            </>
+          )}
         </S.MenuNav>
 
-        <S.RegisterBox>
-          <Button fullWidth size="large">
-            Log in now
-          </Button>
-          <span>or</span>
-          <S.CreateAccount href="#" title="Sign Up">
-            Sign Up
-          </S.CreateAccount>
-        </S.RegisterBox>
+        {!username && (
+          <S.RegisterBox>
+            <Button fullWidth size="large">
+              Log in now
+            </Button>
+            <span>or</span>
+            <S.CreateAccount href="#" title="Sign Up">
+              Sign Up
+            </S.CreateAccount>
+          </S.RegisterBox>
+        )}
       </S.MenuFull>
     </S.Wrapper>
   );
