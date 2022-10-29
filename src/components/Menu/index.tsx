70536cf7055839a955e7954a8872297e2eae6b9a
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-10-29 15:49:30 -0300

    feat(aula161): create logic to open/close Menu with React Hooks and TDD

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,14 +1,18 @@
+import { useState } from 'react';
 import { Menu2 as MenuIcon } from '@styled-icons/remix-fill/Menu2';
 import { ShoppingCart as ShoppingCartIcon } from '@styled-icons/material-outlined/ShoppingCart';
 import { Search as SearchIcon } from '@styled-icons/material-outlined/Search';
+import { Close as CloseIcon } from '@styled-icons/material-outlined/Close';
 
 import { Logo } from 'components/Logo';
 import * as S from './styles';
 
 export const Menu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <S.Wrapper>
-      <S.IconWrapper>
+      <S.IconWrapper onClick={() => setIsOpen(true)}>
         <MenuIcon aria-label="Open Menu" />
       </S.IconWrapper>
       <S.LogoWrapper>
@@ -22,6 +26,10 @@ export const Menu = () => {
           <ShoppingCartIcon aria-label="Open Shopping Cart" />
         </S.IconWrapper>
       </S.MenuGroup>
+
+      <S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
+        <CloseIcon aria-label="Close Menu" onClick={() => setIsOpen(false)} />
+      </S.MenuFull>
     </S.Wrapper>
   );
 };
--- src/components/Menu/test.tsx
+++ src/components/Menu/test.tsx
@@ -1,4 +1,4 @@
-import { screen } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
 import { renderWithTheme } from 'utils/tests/helpers';
 
 import { Menu } from '.';
@@ -10,5 +10,24 @@ describe('<Menu />', () => {
     expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument();
     expect(screen.getByLabelText(/search/i)).toBeInTheDocument();
     expect(screen.getByLabelText(/open shopping cart/i)).toBeInTheDocument();
+  });
+
+  it('should handle the open/close mobile menu', () => {
+    renderWithTheme(<Menu />);
+    // selecionar o nosso menuFull
+    const fullMenuElement = screen.getByRole('navigation', { hidden: true });
+    // verificar se o menu ta escondido
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true');
+    expect(fullMenuElement).toHaveStyle({ opacity: 0 });
+
+    // clicar no botao de abrir o menu e verificar se ele abriu
+    fireEvent.click(screen.getByLabelText(/open menu/i));
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('false');
+    expect(fullMenuElement).toHaveStyle({ opacity: 1 });
+
+    // clicar no botao de fechar o menu e verificar se ele fechou
+    fireEvent.click(screen.getByLabelText(/close menu/i));
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true');
+    expect(fullMenuElement).toHaveStyle({ opacity: 0 });
   });
 });
