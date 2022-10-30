48f1a1bf647d7c538a699b56d62dd55c8589d4fa
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-10-30 18:57:14 -0300

    feat(aula167): add size and lineColors option to Heading component

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -1,10 +1,14 @@
 import * as S from './styles';
 
+export type LineColors = 'primary' | 'secondary';
+
 export type HeadingProps = {
   children: React.ReactNode;
   color?: 'white' | 'black';
   lineLeft?: boolean;
   lineBottom?: boolean;
+  size?: 'small' | 'medium';
+  lineColor?: LineColors;
 };
 
 export const Heading = ({
@@ -12,9 +16,17 @@ export const Heading = ({
   color = 'white',
   lineLeft = false,
   lineBottom = false,
+  lineColor = 'primary',
+  size = 'medium',
 }: HeadingProps) => {
   return (
-    <S.Wrapper color={color} lineLeft={lineLeft} lineBottom={lineBottom}>
+    <S.Wrapper
+      color={color}
+      lineLeft={lineLeft}
+      lineBottom={lineBottom}
+      lineColor={lineColor}
+      size={size}
+    >
       {children}
     </S.Wrapper>
   );
