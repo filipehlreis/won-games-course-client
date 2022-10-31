bf6078ffb500bc02ca397a56a90296cf02e74e73
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-10-30 21:31:30 -0300

    fix(aula170): ajust visual styles when Button works as link a

--- src/components/Button/stories.tsx
+++ src/components/Button/stories.tsx
@@ -31,3 +31,12 @@ withIcon.args = {
   children: 'Buy now',
   icon: <AddShoppingCart />,
 };
+
+export const asLink: Story = (args) => <Button {...args} />;
+
+asLink.args = {
+  size: 'large',
+  children: 'Buy now',
+  as: 'a',
+  href: '/link',
+};
