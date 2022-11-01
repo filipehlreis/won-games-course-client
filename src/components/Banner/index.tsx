ef0891f64ce990f27d3b3ee82e773cec91e67df3
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-10-31 22:07:09 -0300

    feat(aula176e177): put together Ribbon+Banner

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,4 +1,5 @@
 import { Button } from 'components/Button';
+import { Ribbon, RibbonColors, RibbonSizes } from 'components/Ribbon';
 import * as S from './styles';
 
 export type BannerProps = {
@@ -7,6 +8,9 @@ export type BannerProps = {
   subtitle: string;
   buttonLabel: string;
   buttonLink: string;
+  ribbon?: React.ReactNode;
+  ribbonColor?: RibbonColors;
+  ribbonSize?: RibbonSizes;
 };
 
 export const Banner = ({
@@ -15,9 +19,17 @@ export const Banner = ({
   subtitle,
   buttonLabel,
   buttonLink,
+  ribbon,
+  ribbonColor = 'primary',
+  ribbonSize = 'normal',
 }: BannerProps) => {
   return (
     <S.Wrapper>
+      {!!ribbon && (
+        <Ribbon color={ribbonColor} size={ribbonSize}>
+          {ribbon}
+        </Ribbon>
+      )}
       <S.Image src={img} role="img" aria-label={title} />
 
       <S.Caption>
