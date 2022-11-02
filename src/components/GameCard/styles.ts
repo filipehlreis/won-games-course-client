f3a02c2e838f87141785e5c79a77c4a6f3e33016
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-11-02 12:15:16 -0300

    feat(aula185): create styles for GameCard component

--- src/components/GameCard/styles.ts
+++ src/components/GameCard/styles.ts
@@ -1,19 +1,113 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Wrapper = styled.main``;
+export const Wrapper = styled.article`
+  ${({ theme }) => css`
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
+    background-color: ${theme.colors.white};
+  `}
+`;
 
-export const ImageBox = styled.div``;
+export const ImageBox = styled.div`
+  height: 14rem;
+  width: 100%;
+  background: #f6f7f8;
+  background-image: linear-gradient(
+    to right,
+    #f6f7f8 0%,
+    #edeef1 20%,
+    #f6f7f8 40%,
+    #f6f7f8 100%
+  );
+  background-size: 80rem 14rem;
+  animation: placeholderShimmer 1s linear infinite forwards;
 
-export const Content = styled.div``;
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
 
-export const Info = styled.div``;
+  @keyframes placeholderShimmer {
+    0% {
+      background-position: -40rem 0;
+    }
 
-export const Title = styled.h3``;
+    100% {
+      background-position: 40rem 0;
+    }
+  }
+`;
 
-export const Developer = styled.h4``;
+export const Content = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    position: relative;
+    height: 100%;
+    margin: ${theme.spacings.xsmall};
+  `}
+`;
 
-export const FavButton = styled.div``;
+export const Info = styled.div`
+  max-width: calc(100% - 2.5rem);
+`;
 
-export const BuyBox = styled.div``;
+export const Title = styled.h3`
+  ${({ theme }) => css`
+    font-size: ${theme.font.sizes.medium};
+    line-height: ${theme.font.sizes.medium};
+    font-weight: ${theme.font.bold};
+    color: ${theme.colors.black};
+  `}
+`;
 
-export const Price = styled.div``;
+export const Developer = styled.h4`
+  ${({ theme }) => css`
+    font-size: ${theme.font.sizes.small};
+    font-weight: ${theme.font.bold};
+    color: ${theme.colors.gray};
+  `}
+`;
+
+export const FavButton = styled.div`
+  ${({ theme }) => css`
+    color: ${theme.colors.primary};
+    position: absolute;
+    right: 0;
+    top: -0.5rem;
+    cursor: pointer;
+
+    svg {
+      width: 2.5rem;
+    }
+  `}
+`;
+
+export const BuyBox = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    margin-top: ${theme.spacings.xxsmall};
+  `}
+`;
+
+export const Price = styled.div`
+  ${({ theme }) => css`
+    display: inline-flex;
+    font-weight: ${theme.font.bold};
+    height: 3rem;
+    align-items: center;
+
+    color: ${theme.colors.white};
+    padding: 0 ${theme.spacings.xxsmall};
+    background-color: ${theme.colors.secondary};
+    border-radius: ${theme.border.radius};
+    margin-right: calc(${theme.spacings.xxsmall} / 2);
+  `}
+`;
