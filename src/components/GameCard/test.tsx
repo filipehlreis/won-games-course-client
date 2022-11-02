7e50e94de76917c36b0645065853424f785a81aa
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-11-02 17:15:16 -0300

    test(aula187): correct some tests about GameCard component

--- src/components/GameCard/test.tsx
+++ src/components/GameCard/test.tsx
@@ -10,62 +10,43 @@ const props = {
   price: 'R$ 235,00',
 };
 
-const promotionalPrice = 'R$ 199,90';
-
 describe('<GameCard />', () => {
   it('should render the heading', () => {
-    // renderizar o GameCard
     renderWithTheme(<GameCard {...props} />);
-    // verificar se o title foi renderizado
+
     expect(
       screen.getByRole('heading', { name: props.title }),
     ).toBeInTheDocument();
 
-    // verificar se o developer foi renderizado
     expect(
       screen.getByRole('heading', { name: props.developer }),
     ).toBeInTheDocument();
 
-    // verificar se o img foi renderizado
     expect(screen.getByRole('img', { name: props.title })).toHaveAttribute(
       'src',
       props.img,
     );
 
-    // verificar se o price foi renderizado
     expect(screen.getByText(`${props.price}`)).toBeInTheDocument();
-
-    // verificar se o botaozinho foi renderizado
     expect(screen.getByLabelText(/add to wishlist/i)).toBeInTheDocument();
   });
 
   it('should render price in label', () => {
-    // renderiza o componente
     renderWithTheme(<GameCard {...props} />);
+    const price = screen.getByText(props.price);
 
-    // preco nao tenha o corte line-through
-    expect(screen.getByText(`${props.price}`)).toHaveStyle(
-      'text-decoration: none',
-    );
-    // preco tenha o background secundario
-    expect(screen.getByText(`${props.price}`)).toHaveStyle(
-      'background: #3CD3C1',
-    );
+    expect(price).not.toHaveStyle({ textDecoration: 'line-through' });
+    expect(price).toHaveStyle('backgroundColor: #3CD3C1');
   });
 
   it('should render a line-through in price when promotional', () => {
-    // renderiza o componente (Com promotionalPrice) // 200 reais // 15 reais
-    renderWithTheme(
-      <GameCard {...props} promotionalPrice={promotionalPrice} />,
-    );
-    // preco tenha o corte line-through (200)
-    expect(screen.getByText(`${props.price}`)).toHaveStyle(
-      'text-decoration: line-through',
-    );
+    renderWithTheme(<GameCard {...props} promotionalPrice="R$ 199,00" />);
+    const price = screen.getByText(props.price);
+    const promotionalPrice = screen.getByText('R$ 199,00');
 
-    // preco novo promocional nao vai ter line-through (15)
-    expect(screen.getByText(`${promotionalPrice}`)).toHaveStyle(
-      'text-decoration: none',
-    );
+    expect(price).toHaveStyle({ textDecoration: 'line-through' });
+    expect(promotionalPrice).not.toHaveStyle({
+      textDecoration: 'line-through',
+    });
   });
 });
