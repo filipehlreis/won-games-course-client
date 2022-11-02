7cb4a7052e5c39b48bedd5ce6913dfbeed4f5e4c
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-11-01 21:33:48 -0300

    feat(aula179): create initial structure for Highlight component

--- src/components/Highlight/index.tsx
+++ src/components/Highlight/index.tsx
@@ -1,9 +1,26 @@
+import { Button } from 'components/Button';
 import * as S from './styles';
 
-export const Highlight = () => {
+export type HighlightProps = {
+  title: string;
+  subtitle: string;
+  buttonLabel: string;
+  buttonLink: string;
+};
+
+export const Highlight = ({
+  title,
+  subtitle,
+  buttonLabel,
+  buttonLink,
+}: HighlightProps) => {
   return (
     <S.Wrapper>
-      <h1>Highlight</h1>
+      <S.Title>{title}</S.Title>
+      <S.Subtitle>{subtitle}</S.Subtitle>
+      <Button as="a" href={buttonLink}>
+        {buttonLabel}
+      </Button>
     </S.Wrapper>
   );
 };
