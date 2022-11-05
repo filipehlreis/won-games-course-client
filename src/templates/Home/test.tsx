01224a3f0c973d5a5539a39bdf6d353ebdcbbf9d
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-11-04 23:26:20 -0300

    test(aula208): optimize tests for Home page

--- src/templates/Home/test.tsx
+++ src/templates/Home/test.tsx
@@ -10,13 +10,13 @@ import { Home } from '.';
 
 const props = {
   banners: bannersMock,
-  newGames: gamesMock,
+  newGames: [gamesMock[0]],
   mostPopularHighlight: highlightMock,
-  mostPopularGames: gamesMock,
-  upcommingGames: gamesMock,
+  mostPopularGames: [gamesMock[0]],
+  upcommingGames: [gamesMock[0]],
   upcommingHighlight: highlightMock,
-  upcommingMoreGames: gamesMock,
-  freeGames: gamesMock,
+  upcommingMoreGames: [gamesMock[0]],
+  freeGames: [gamesMock[0]],
   freeHighlight: highlightMock,
 };
 
@@ -24,15 +24,17 @@ describe('<Home />', () => {
   it('should render menu and footer', () => {
     renderWithTheme(<Home {...props} />);
 
+    // menu
     expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument();
+    //fotter
     expect(
       screen.getByRole('heading', { name: /contact us/i }),
     ).toBeInTheDocument();
-  });
 
-  it('should render the sections', () => {
-    renderWithTheme(<Home {...props} />);
+    // logos (menu / footer)
+    expect(screen.getAllByRole('img', { name: /won games/i })).toHaveLength(2);
 
+    // should render the sections
     expect(screen.getByRole('heading', { name: /news/i })).toBeInTheDocument();
     expect(
       screen.getByRole('heading', { name: /most popular/i }),
@@ -43,14 +45,13 @@ describe('<Home />', () => {
     expect(
       screen.getByRole('heading', { name: /free games/i }),
     ).toBeInTheDocument();
-  });
 
-  it('should render section elements', () => {
-    renderWithTheme(<Home {...props} />);
+    // should render section elements
     // banner
     expect(screen.getAllByText(/defy death 1/i)).toHaveLength(1);
     // card game( 5sections com 4 cards cada = 5x4 = 20)
-    expect(screen.getAllByText(/population zero/i)).toHaveLength(20);
+    // card game( 5sections com 1 cards cada = 5x1 = 5)
+    expect(screen.getAllByText(/population zero/i)).toHaveLength(5);
     // highlight
     expect(screen.getAllByText(/read dead it's back/i)).toHaveLength(3);
   });
