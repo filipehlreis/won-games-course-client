c1a7966d535bafeeed287110c2d16416d7709465
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-11-05 17:09:19 -0300

    feat(aula211): add minimal style option to Button component

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,9 +1,10 @@
+import { darken } from 'polished';
 import styled, { css, DefaultTheme } from 'styled-components';
 import { ButtonProps } from '.';
 
 type WrapperProps = {
   hasIcon: boolean;
-} & Pick<ButtonProps, 'size' | 'fullWidth'>;
+} & Pick<ButtonProps, 'size' | 'fullWidth' | 'minimal'>;
 
 const wrapperModifiers = {
   small: (theme: DefaultTheme) => css`
@@ -33,10 +34,20 @@ const wrapperModifiers = {
       }
     }
   `,
+
+  minimal: (theme: DefaultTheme) => css`
+    background: none;
+    color: ${theme.colors.primary};
+
+    &:hover {
+      color: ${darken(0.1, theme.colors.primary)};
+      background: none;
+    }
+  `,
 };
 
 export const Wrapper = styled.button<WrapperProps>`
-  ${({ theme, size, fullWidth, hasIcon }) => css`
+  ${({ theme, size, fullWidth, hasIcon, minimal }) => css`
     display: inline-flex;
     align-items: center;
     justify-content: center;
@@ -50,11 +61,12 @@ export const Wrapper = styled.button<WrapperProps>`
     text-decoration: none;
 
     &:hover {
-      background: linear-gradient(180deg, #e35565 0%, #d958a6 50%);
+      background: linear-gradient(180deg, #e35565 0%, #d958a6 50%)};
     }
 
     ${!!size && wrapperModifiers[size](theme)}
     ${!!fullWidth && wrapperModifiers.fullWidth()}
     ${!!hasIcon && wrapperModifiers.withIcon(theme)}
+    ${!!minimal && wrapperModifiers.minimal(theme)}
   `}
 `;
