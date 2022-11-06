77a3c2c21003220ab7b4e4f7869b82017780a26d
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-11-05 22:13:25 -0300

    feat(aula218): add onChange method to Checkbox component and more tests

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -1,19 +1,39 @@
+import { InputHTMLAttributes, useState } from 'react';
 import * as S from './styles';
 
 export type CheckboxProps = {
+  onCheck?: (status: boolean) => void;
   label?: string;
   labelFor?: string;
   labelColor?: 'black' | 'white';
-};
+} & InputHTMLAttributes<HTMLInputElement>;
 
 export const Checkbox = ({
+  onCheck,
   label,
   labelFor = '',
   labelColor = 'white',
 }: CheckboxProps) => {
+  // controlled component (state)
+  const [checked, setChecked] = useState(false);
+
+  const onChange = () => {
+    const status = !checked;
+    setChecked(status);
+
+    if (onCheck) {
+      onCheck(status);
+    }
+  };
+
   return (
     <S.Wrapper>
-      <S.Input id={labelFor} type="checkbox" />
+      <S.Input
+        id={labelFor}
+        type="checkbox"
+        onChange={onChange}
+        checked={checked}
+      />
       {!!label && (
         <S.Label htmlFor={labelFor} labelColor={labelColor}>
           {label}
