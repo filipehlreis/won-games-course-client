bb2507d77bb022d8a5cf5a69416cc9e85aa8406d
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-11-06 21:35:41 -0300

    feat(aula230): create initial template structure and initial tests

--- src/templates/Auth/index.tsx
+++ src/templates/Auth/index.tsx
@@ -1,9 +1,33 @@
+import { Heading } from 'components/Heading';
+import { Logo } from 'components/Logo';
 import * as S from './styles';
 
-export const Auth = () => {
+type AuthProps = {
+  title: string;
+  children: React.ReactNode;
+};
+
+export const Auth = ({ title, children }: AuthProps) => {
   return (
     <S.Wrapper>
-      <h1>Auth</h1>
+      <S.BannerBlock>
+        <Logo />
+
+        <Heading>All your favorite games in one place</Heading>
+        <S.Subtitle>
+          <strong>WON</strong> is the best and most complete gaming platform.
+        </S.Subtitle>
+        <S.Footer>Won Games 2020 © Todos os Direitos Reservados</S.Footer>
+      </S.BannerBlock>
+
+      <S.Content>
+        <Logo color="black" size="large" />
+        <Heading color="black" lineLeft lineColor="secondary">
+          {title}
+        </Heading>
+
+        {children}
+      </S.Content>
     </S.Wrapper>
   );
 };
--- src/templates/Auth/test.tsx
+++ src/templates/Auth/test.tsx
@@ -1,11 +1,35 @@
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
+import { renderWithTheme } from 'utils/tests/helpers';
 
 import { Auth } from '.';
 
 describe('<Auth />', () => {
-  it('should render the heading', () => {
-    render(<Auth />);
+  it('should render all components and children', () => {
+    renderWithTheme(
+      <Auth title="Auth Title">
+        <input type="text" />
+      </Auth>,
+    );
 
-    expect(screen.getByRole('heading', { name: /Auth/i })).toBeInTheDocument();
+    // verifica se tem 2 logos
+    expect(screen.getAllByRole('img', { name: /won games/i })).toHaveLength(2);
+    // verifica se tem o heading principal do banner
+    expect(
+      screen.getByRole('heading', {
+        name: 'All your favorite games in one place',
+      }),
+    ).toBeInTheDocument();
+    // verifica se tem o subtitle
+    expect(
+      screen.getByRole('heading', {
+        name: 'WON is the best and most complete gaming platform.',
+      }),
+    ).toBeInTheDocument();
+    // verifica se tem o title do content
+    expect(
+      screen.getByRole('heading', { name: 'Auth Title' }),
+    ).toBeInTheDocument();
+    // verifica se o children ta sendo renderizado
+    expect(screen.getByRole('textbox')).toBeInTheDocument();
   });
 });
