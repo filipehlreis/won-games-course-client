8313b45b21798ad9652ef3165229680f2dfbfb92
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-11-12 23:24:28 -0300

    feat(aula257): formate Rating and Genres and more tests

--- src/components/GameDetails/test.tsx
+++ src/components/GameDetails/test.tsx
@@ -7,6 +7,8 @@ const props: GameDetailsProps = {
   developer: 'Different Tales',
   platforms: ['windows', 'linux', 'mac'],
   releaseDate: '2020-11-21T23:00:00',
+  rating: 'BR0',
+  genres: ['Role-playing', 'Narrative'],
 };
 
 describe('<GameDetails />', () => {
@@ -41,9 +43,51 @@ describe('<GameDetails />', () => {
     expect(screen.getByRole('img', { name: /mac/i })).toBeInTheDocument();
   });
 
+  it('should render FREE rating when BR0', () => {
+    renderWithTheme(<GameDetails {...props} />);
+
+    expect(screen.getByText(/free/i)).toBeInTheDocument();
+  });
+
+  it('should render 10+ rating when BR10', () => {
+    renderWithTheme(<GameDetails {...props} rating="BR10" />);
+
+    expect(screen.getByText(/10\+/i)).toBeInTheDocument();
+  });
+
+  it('should render 12+ rating when BR12', () => {
+    renderWithTheme(<GameDetails {...props} rating="BR12" />);
+
+    expect(screen.getByText(/12\+/i)).toBeInTheDocument();
+  });
+
+  it('should render 14+ rating when BR14', () => {
+    renderWithTheme(<GameDetails {...props} rating="BR14" />);
+
+    expect(screen.getByText(/14\+/i)).toBeInTheDocument();
+  });
+
+  it('should render 16+ rating when BR16', () => {
+    renderWithTheme(<GameDetails {...props} rating="BR16" />);
+
+    expect(screen.getByText(/16\+/i)).toBeInTheDocument();
+  });
+
+  it('should render 18+ rating when BR18', () => {
+    renderWithTheme(<GameDetails {...props} rating="BR18" />);
+
+    expect(screen.getByText(/18\+/i)).toBeInTheDocument();
+  });
+
   it('should render the formated date', () => {
     renderWithTheme(<GameDetails {...props} />);
 
     expect(screen.getByText('Nov 21, 2020')).toBeInTheDocument();
+  });
+
+  it('should render a list of genres', () => {
+    renderWithTheme(<GameDetails {...props} />);
+
+    expect(screen.getByText(/role-playing \/ narrative/i)).toBeInTheDocument();
   });
 });
