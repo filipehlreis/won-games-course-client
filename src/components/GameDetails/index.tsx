e4f435aad37411673247a512a2f528a9aa7afbf3
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-11-12 21:31:03 -0300

    feat(aula256): create Tests and formate datetime with Intl

--- src/components/GameDetails/index.tsx
+++ src/components/GameDetails/index.tsx
@@ -8,10 +8,16 @@ import * as S from './styles';
 type Platform = 'windows' | 'linux' | 'mac';
 
 export type GameDetailsProps = {
+  developer: string;
   platforms: Platform[];
+  releaseDate: string;
 };
 
-const GameDetails = ({ platforms }: GameDetailsProps) => {
+const GameDetails = ({
+  developer,
+  platforms,
+  releaseDate,
+}: GameDetailsProps) => {
   const platformIcons = {
     linux: <Linux title="Linux" size={18} />,
     windows: <Windows title="Windows" size={18} />,
@@ -29,12 +35,18 @@ const GameDetails = ({ platforms }: GameDetailsProps) => {
       <S.Content>
         <S.Block>
           <S.Label>Developer</S.Label>
-          <S.Description>Gearbox Software</S.Description>
+          <S.Description>{developer}</S.Description>
         </S.Block>
 
         <S.Block>
           <S.Label>Release Date</S.Label>
-          <S.Description>Nov 16, 2019</S.Description>
+          <S.Description>
+            {new Intl.DateTimeFormat('en-US', {
+              day: 'numeric',
+              month: 'short',
+              year: 'numeric',
+            }).format(new Date(releaseDate))}
+          </S.Description>
         </S.Block>
 
         <S.Block>
--- src/components/GameDetails/test.tsx
+++ src/components/GameDetails/test.tsx
@@ -1,10 +1,49 @@
-// import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import { renderWithTheme } from 'utils/tests/helpers';
 
-import GameDetails from '.';
+import GameDetails, { GameDetailsProps } from '.';
+
+const props: GameDetailsProps = {
+  developer: 'Different Tales',
+  platforms: ['windows', 'linux', 'mac'],
+  releaseDate: '2020-11-21T23:00:00',
+};
 
 describe('<GameDetails />', () => {
-  it('should render the heading', () => {
-    renderWithTheme(<GameDetails platforms={[]} />);
+  it('should render the blocks', () => {
+    renderWithTheme(<GameDetails {...props} />);
+
+    expect(
+      screen.getByRole('heading', { name: /developer/i }),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole('heading', { name: /release date/i }),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole('heading', { name: /platforms/i }),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole('heading', { name: /publisher/i }),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole('heading', { name: /rating/i }),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole('heading', { name: /genres/i }),
+    ).toBeInTheDocument();
+  });
+
+  it('should render icons platform', () => {
+    renderWithTheme(<GameDetails {...props} />);
+
+    expect(screen.getByRole('img', { name: /windows/i })).toBeInTheDocument();
+    expect(screen.getByRole('img', { name: /linux/i })).toBeInTheDocument();
+    expect(screen.getByRole('img', { name: /mac/i })).toBeInTheDocument();
+  });
+
+  it('should render the formated date', () => {
+    renderWithTheme(<GameDetails {...props} />);
+
+    expect(screen.getByText('Nov 21, 2020')).toBeInTheDocument();
   });
 });
