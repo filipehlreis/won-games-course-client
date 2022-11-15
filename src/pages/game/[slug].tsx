f4b0a00f6c3c32b48fb41f1a77a00b2ee7581a82
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-11-14 21:45:12 -0300

    feat(aula273): add GameInfo component to Game template

--- src/pages/game/[slug].tsx
+++ src/pages/game/[slug].tsx
@@ -17,6 +17,12 @@ export async function getStaticProps() {
     props: {
       cover:
         'https://raw.githubusercontent.com/Won-Games/client/48dc534d44bcb0f0175e373093d3c85bc178a353/public/img/games/cyberpunk-1.jpg',
+      gameInfo: {
+        title: 'Cyberpunk 2077',
+        price: '59.00',
+        description:
+          'Cyberpunk 2077 is an open-world, action-adventure story set in Night City, a megalopolis obsessed with power, glamour and body modification. You play as V, a mercenary outlaw going after a one-of-a-kind implant that is the key to immortality',
+      },
     },
   };
 }
--- src/templates/Game/index.tsx
+++ src/templates/Game/index.tsx
@@ -1,15 +1,24 @@
 import Base from 'templates/Base';
 
+import { GameInfo, GameInfoProps } from 'components/GameInfo';
+
 import * as S from './styles';
 
 export type GameTemplateProps = {
   cover: string;
+  gameInfo: GameInfoProps;
 };
 
-const Game = ({ cover }: GameTemplateProps) => {
+const Game = ({ cover, gameInfo }: GameTemplateProps) => {
   return (
     <Base>
       <S.Cover src={cover} role="image" aria-label="cover" />
+
+      <S.Main>
+        <S.SectionGameInfo>
+          <GameInfo {...gameInfo} />
+        </S.SectionGameInfo>
+      </S.Main>
     </Base>
   );
 };
