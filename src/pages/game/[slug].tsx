bdf09c3d16616f1bc58a602d5fe0e681bc16a563
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-11-14 21:12:07 -0300

    feat(aula269e270): create Game template and adjust Base layout

--- src/pages/game/[slug].tsx
+++ src/pages/game/[slug].tsx
@@ -1,7 +1,5 @@
-import { useRouter } from 'next/router';
+import Game from 'templates/Game';
 
 export default function Index() {
-  const router = useRouter();
-
-  return <h1>{router.asPath}</h1>;
+  return <Game />;
 }
---
+++ src/templates/Game/index.tsx
@@ -0,0 +1,13 @@
+import Base from 'templates/Base';
+
+// import * as S from './styles';
+
+const Game = () => {
+  return (
+    <Base>
+      <h1>Game</h1>
+    </Base>
+  );
+};
+
+export default Game;
