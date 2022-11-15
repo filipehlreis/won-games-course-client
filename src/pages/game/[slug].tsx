3693f9536514ea154dd8273fbc8ebc3f68007914
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-11-14 21:32:20 -0300

    feat(aula272): learn how to use getStaticProps and getStaticPaths

--- src/pages/game/[slug].tsx
+++ src/pages/game/[slug].tsx
@@ -1,5 +1,22 @@
-import Game from 'templates/Game';
+import Game, { GameTemplateProps } from 'templates/Game';
 
-export default function Index() {
-  return <Game />;
+export default function Index(props: GameTemplateProps) {
+  return <Game {...props} />;
+}
+
+// gerar em build time (/game/bla, /game/foo ...)
+export async function getStaticPaths() {
+  return {
+    paths: [{ params: { slug: 'cyberpunk-2077' } }],
+    fallback: false,
+  };
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      cover:
+        'https://raw.githubusercontent.com/Won-Games/client/48dc534d44bcb0f0175e373093d3c85bc178a353/public/img/games/cyberpunk-1.jpg',
+    },
+  };
 }
--- src/templates/Game/index.tsx
+++ src/templates/Game/index.tsx
@@ -2,14 +2,14 @@ import Base from 'templates/Base';
 
 import * as S from './styles';
 
-const Game = () => {
+export type GameTemplateProps = {
+  cover: string;
+};
+
+const Game = ({ cover }: GameTemplateProps) => {
   return (
     <Base>
-      <S.Cover
-        src="https://raw.githubusercontent.com/Won-Games/client/48dc534d44bcb0f0175e373093d3c85bc178a353/public/img/games/cyberpunk-1.jpg"
-        role="image"
-        aria-label="cover"
-      />
+      <S.Cover src={cover} role="image" aria-label="cover" />
     </Base>
   );
 };
