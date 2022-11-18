8a2ca1bf9e4c71ca69032ddf57caf92103a3674e
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-11-18 20:38:28 -0300

    feat(aula289): create GameItem component and initial test

---
+++ src/components/GameItem/test.tsx
@@ -0,0 +1,28 @@
+import { screen } from '@testing-library/react';
+import { renderWithTheme } from 'utils/tests/helpers';
+
+import GameItem from '.';
+
+const props = {
+  img: 'https://source.unsplash.com/user/willianjusten/151x70',
+  title: 'Red Dead Redemption 2',
+  price: 'R$ 215,00',
+};
+
+describe('<GameItem />', () => {
+  it('should render the item', () => {
+    renderWithTheme(<GameItem {...props} />);
+
+    // verificar se o title se foi renderizado
+    expect(
+      screen.getByRole('heading', { name: props.title }),
+    ).toBeInTheDocument();
+    // verificar a imagem
+    expect(screen.getByRole('img', { name: props.title })).toHaveAttribute(
+      'src',
+      props.img,
+    );
+    // verificar o preco
+    expect(screen.getByText('R$ 215,00')).toBeInTheDocument();
+  });
+});
