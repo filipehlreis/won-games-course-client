0fe76adeba7496ece7f8e901555ce96a75e3f543
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-11-18 17:37:19 -0300

    feat(aula288): add Empty component to Wishlist template page

--- src/templates/Wishlist/index.tsx
+++ src/templates/Wishlist/index.tsx
@@ -7,6 +7,7 @@ import { Container } from 'components/Container';
 import { Grid } from 'components/Grid';
 import { HighlightProps } from 'components/Highlight';
 import { Divider } from 'components/Divider';
+import Empty from 'components/Empty';
 
 export type WishlistTemplateProps = {
   games?: GameCardProps[];
@@ -15,7 +16,7 @@ export type WishlistTemplateProps = {
 };
 
 const Wishlist = ({
-  games,
+  games = [],
   recommendedGames,
   recommendedHighlight,
 }: WishlistTemplateProps) => {
@@ -26,11 +27,19 @@ const Wishlist = ({
           Wishlist
         </Heading>
 
-        <Grid>
-          {games?.map((game, index) => (
-            <GameCard key={`wishlist-${index}`} {...game} />
-          ))}
-        </Grid>
+        {games.length ? (
+          <Grid>
+            {games?.map((game, index) => (
+              <GameCard key={`wishlist-${index}`} {...game} />
+            ))}
+          </Grid>
+        ) : (
+          <Empty
+            title="Your wishlist is empty"
+            description="Games added to your wishlist will appear here"
+            hasLink
+          />
+        )}
 
         <Divider />
       </Container>
