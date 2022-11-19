cab19081d65212a64184ecad56b7be35c6817d91
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-11-19 19:38:01 -0300

    feat(aula297e298): create Cart Page template, initial structure, styles and tests

---
+++ src/templates/Cart/test.tsx
@@ -0,0 +1,74 @@
+import 'match-media-mock';
+import { screen } from '@testing-library/react';
+import { renderWithTheme } from 'utils/tests/helpers';
+
+import itemsMock from 'components/CartList/mock';
+import gamesMock from 'components/GameCardSlider/mock';
+import highlightMock from 'components/Highlight/mock';
+import cardsMock from 'components/PaymentOptions/mock';
+
+import Cart from '.';
+import React from 'react';
+
+const props = {
+  items: itemsMock,
+  total: 'R$ 430,00',
+  cards: cardsMock,
+  recommendedGames: gamesMock,
+  recommendedHighlight: highlightMock,
+};
+
+jest.mock('templates/Base', () => ({
+  __esModule: true,
+  default: function Mock({ children }: { children: React.ReactNode }) {
+    return <div data-testid="Mock Base">{children}</div>;
+  },
+}));
+
+jest.mock('components/Showcase', () => ({
+  __esModule: true,
+  default: function Mock() {
+    return <div data-testid="Mock Showcase" />;
+  },
+}));
+
+jest.mock('components/CartList', () => ({
+  __esModule: true,
+  default: function Mock() {
+    return <div data-testid="Mock Cart" />;
+  },
+}));
+
+jest.mock('components/PaymentOptions', () => ({
+  __esModule: true,
+  default: function Mock() {
+    return <div data-testid="Mock PaymentOptions" />;
+  },
+}));
+
+jest.mock('components/Empty', () => ({
+  __esModule: true,
+  default: function Mock() {
+    return <div data-testid="Mock Empty" />;
+  },
+}));
+
+describe('<Cart />', () => {
+  it('should render sections', () => {
+    renderWithTheme(<Cart {...props} />);
+
+    expect(
+      screen.getByRole('heading', { name: /my cart/i }),
+    ).toBeInTheDocument();
+    expect(screen.getByTestId('Mock Cart')).toBeInTheDocument();
+    expect(screen.getByTestId('Mock PaymentOptions')).toBeInTheDocument();
+    expect(screen.getByTestId('Mock Showcase')).toBeInTheDocument();
+    expect(screen.queryByTestId('Mock Empty')).not.toBeInTheDocument();
+  });
+
+  it('should render empty section if there are no items', () => {
+    renderWithTheme(<Cart {...props} items={[]} />);
+
+    expect(screen.getByTestId('Mock Empty')).toBeInTheDocument();
+  });
+});
