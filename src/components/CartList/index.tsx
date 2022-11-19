944f9aeb8d596d270a495579b0ed86287a6f37ec
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-11-19 13:36:11 -0300

    feat(aula293): create CartList component, styles and tests

---
+++ src/components/CartList/index.tsx
@@ -0,0 +1,23 @@
+import GameItem, { GameItemProps } from 'components/GameItem';
+import * as S from './styles';
+
+export type CartListProps = {
+  items: GameItemProps[];
+  total: string;
+};
+
+const CartList = ({ items, total }: CartListProps) => {
+  return (
+    <S.Wrapper>
+      {items.map((item) => (
+        <GameItem key={item.title} {...item} />
+      ))}
+
+      <S.Footer>
+        Total <S.Total>{total}</S.Total>
+      </S.Footer>
+    </S.Wrapper>
+  );
+};
+
+export default CartList;
---
+++ src/components/CartList/stories.tsx
@@ -0,0 +1,31 @@
+import { Story, Meta } from '@storybook/react/types-6-0';
+import CartList, { CartListProps } from '.';
+
+import mockItems from './mock';
+
+export default {
+  title: 'CartList',
+  component: CartList,
+  args: {
+    items: mockItems,
+    total: 'R$ 330,00',
+  },
+  parameters: {
+    backgrounds: {
+      default: 'won-dark',
+    },
+  },
+  argTypes: {
+    items: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+} as Meta;
+
+export const Default: Story<CartListProps> = (args) => (
+  <div style={{ maxWidth: 800 }}>
+    <CartList {...args} />
+  </div>
+);
