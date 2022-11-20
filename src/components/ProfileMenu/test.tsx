b8f48b38cdca4dc0fec6b4514aa6de7435735512
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-11-19 21:52:31 -0300

    style(prettier): fix prettier config

--- src/components/ProfileMenu/test.tsx
+++ src/components/ProfileMenu/test.tsx
@@ -7,10 +7,14 @@ describe('<ProfileMenu />', () => {
   it('should render the menu', () => {
     const { container } = renderWithTheme(<ProfileMenu />);
 
-    expect(screen.getByRole('link', { name: /my profile/i })).toBeInTheDocument();
+    expect(
+      screen.getByRole('link', { name: /my profile/i }),
+    ).toBeInTheDocument();
 
     expect(screen.getByRole('link', { name: /my cards/i })).toBeInTheDocument();
-    expect(screen.getByRole('link', { name: /my orders/i })).toBeInTheDocument();
+    expect(
+      screen.getByRole('link', { name: /my orders/i }),
+    ).toBeInTheDocument();
     expect(screen.getByRole('link', { name: /sign out/i })).toBeInTheDocument();
 
     expect(container.firstChild).toMatchSnapshot();
