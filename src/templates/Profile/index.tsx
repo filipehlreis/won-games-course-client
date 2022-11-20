ca3c57a22c7c1c3b7ce8e8d2258b61e016656b57
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-11-20 16:43:08 -0300

    feat(aula307e308): create Profile Template page and profile/me and profile/cards page

---
+++ src/templates/Profile/index.tsx
@@ -0,0 +1,28 @@
+import { Container } from 'components/Container';
+import { Heading } from 'components/Heading';
+import ProfileMenu from 'components/ProfileMenu';
+import Base from 'templates/Base';
+import * as S from './styles';
+
+export type ProfileTemplateProps = {
+  children: React.ReactNode;
+};
+
+const Profile = ({ children }: ProfileTemplateProps) => {
+  return (
+    <Base>
+      <Container>
+        <Heading lineLeft lineColor="secondary">
+          My profile
+        </Heading>
+
+        <S.Main>
+          <ProfileMenu />
+          <S.Content>{children}</S.Content>
+        </S.Main>
+      </Container>
+    </Base>
+  );
+};
+
+export default Profile;
