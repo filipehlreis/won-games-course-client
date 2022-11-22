f612bc78a3155c45b0083fe9f56cb301b04ed642
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-11-21 22:39:41 -0300

    feat(aula315): create onFilter method for ExploreSidebar component

--- src/components/ExploreSidebar/index.tsx
+++ src/components/ExploreSidebar/index.tsx
@@ -2,6 +2,7 @@ import Button from 'components/Button';
 import { Checkbox } from 'components/Checkbox';
 import Heading from 'components/Heading';
 import { Radio } from 'components/Radio';
+import { useState } from 'react';
 import * as S from './styles';
 
 export type ItemProps = {
@@ -16,11 +17,31 @@ type Field = {
   name: string;
 };
 
-export type ExploreSidebarProps = {
-  items: ItemProps[];
+type Values = {
+  [field: string]: boolean | string;
 };
 
-const ExploreSidebar = ({ items }: ExploreSidebarProps) => {
+export type ExploreSidebarProps = {
+  items: ItemProps[];
+  initialValues?: Values;
+  onFilter: (values: Values) => void;
+};
+
+const ExploreSidebar = ({
+  items,
+  onFilter,
+  initialValues = {},
+}: ExploreSidebarProps) => {
+  const [values, setValues] = useState(initialValues);
+
+  const handleChange = (name: string, value: string | boolean) => {
+    setValues((s) => ({ ...s, [name]: value }));
+  };
+
+  const handleFilter = () => {
+    onFilter(values);
+  };
+
   return (
     <S.Wrapper>
       {items.map((item) => (
@@ -36,6 +57,8 @@ const ExploreSidebar = ({ items }: ExploreSidebarProps) => {
                 name={field.name}
                 label={field.label}
                 labelFor={field.name}
+                isChecked={!!values[field.name]}
+                onCheck={(v) => handleChange(field.name, v)}
               />
             ))}
           {item.type === 'radio' &&
@@ -47,12 +70,14 @@ const ExploreSidebar = ({ items }: ExploreSidebarProps) => {
                 label={field.label}
                 labelFor={field.name}
                 value={field.name}
+                defaultChecked={field.name === values[item.name]}
+                onChange={() => handleChange(item.name, field.name)}
               />
             ))}
         </div>
       ))}
 
-      <Button fullWidth size="medium">
+      <Button fullWidth size="medium" onClick={handleFilter}>
         Filter
       </Button>
     </S.Wrapper>
--- src/components/ExploreSidebar/test.tsx
+++ src/components/ExploreSidebar/test.tsx
@@ -1,4 +1,5 @@
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { renderWithTheme } from 'utils/tests/helpers';
 
 import ExploreSidebar from '.';
@@ -7,7 +8,7 @@ import items from './mock';
 
 describe('<ExploreSidebar />', () => {
   it('should render headings', () => {
-    renderWithTheme(<ExploreSidebar items={items} />);
+    renderWithTheme(<ExploreSidebar items={items} onFilter={jest.fn} />);
 
     expect(screen.getByRole('heading', { name: /price/i })).toBeInTheDocument();
     expect(
@@ -20,7 +21,7 @@ describe('<ExploreSidebar />', () => {
   });
 
   it('should render inputs', () => {
-    renderWithTheme(<ExploreSidebar items={items} />);
+    renderWithTheme(<ExploreSidebar items={items} onFilter={jest.fn} />);
 
     expect(
       screen.getByRole('checkbox', { name: /under \$50/i }),
@@ -31,8 +32,65 @@ describe('<ExploreSidebar />', () => {
   });
 
   it('should render the filter button', () => {
-    renderWithTheme(<ExploreSidebar items={items} />);
+    renderWithTheme(<ExploreSidebar items={items} onFilter={jest.fn} />);
 
     expect(screen.getByRole('button', { name: /filter/i })).toBeInTheDocument();
+  });
+
+  it('should check initial values that are passed', () => {
+    renderWithTheme(
+      <ExploreSidebar
+        items={items}
+        initialValues={{ windows: true, sort_by: 'low-to-high' }}
+        onFilter={jest.fn}
+      />,
+    );
+
+    expect(screen.getByRole('checkbox', { name: /windows/i })).toBeChecked();
+    expect(screen.getByRole('radio', { name: /low to high/i })).toBeChecked();
+  });
+
+  it('should filter with initial values', async () => {
+    const onFilter = jest.fn();
+
+    renderWithTheme(
+      <ExploreSidebar
+        items={items}
+        initialValues={{ windows: true, sort_by: 'low-to-high' }}
+        onFilter={onFilter}
+      />,
+    );
+
+    await userEvent.click(screen.getByRole('button', { name: /filter/i }));
+    expect(onFilter).toBeCalledWith({ windows: true, sort_by: 'low-to-high' });
+  });
+
+  it('should filter with checked values', async () => {
+    const onFilter = jest.fn();
+
+    renderWithTheme(<ExploreSidebar items={items} onFilter={onFilter} />);
+
+    await userEvent.click(screen.getByLabelText(/windows/i));
+    await userEvent.click(screen.getByLabelText(/linux/i));
+    await userEvent.click(screen.getByLabelText(/low to high/i));
+
+    await userEvent.click(screen.getByRole('button', { name: /filter/i }));
+    expect(onFilter).toBeCalledWith({
+      windows: true,
+      linux: true,
+      sort_by: 'low-to-high',
+    });
+  });
+
+  it('should altern between radio options', async () => {
+    const onFilter = jest.fn();
+
+    renderWithTheme(<ExploreSidebar items={items} onFilter={onFilter} />);
+
+    await userEvent.click(screen.getByLabelText(/low to high/i));
+    await userEvent.click(screen.getByLabelText(/high to low/i));
+
+    await userEvent.click(screen.getByRole('button', { name: /filter/i }));
+    expect(onFilter).toBeCalledWith({ sort_by: 'high-to-low' });
   });
 });
