80e311e2823ac15efba7100189f66296d2a9167f
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-11-24 21:36:08 -0300

    feat(aula320): create CartIcon component, styles and tests

---
+++ src/components/CartIcon/index.tsx
@@ -0,0 +1,18 @@
+import { ShoppingCart } from '@styled-icons/material-outlined';
+
+import * as S from './styles';
+
+export type CartIconProps = {
+  quantity?: number;
+};
+
+const CartIcon = ({ quantity = 0 }: CartIconProps) => {
+  return (
+    <S.Wrapper>
+      {quantity > 0 && <S.Badge aria-label="Cart Items">{quantity}</S.Badge>}
+      <ShoppingCart aria-label="Shopping Cart" />
+    </S.Wrapper>
+  );
+};
+
+export default CartIcon;
