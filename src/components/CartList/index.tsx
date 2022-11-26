2c93422e234df3f8388ea44c83b5e46356d72a93
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-11-26 18:53:13 -0300

    feat(aula324): add button option to CartList component

--- src/components/CartList/index.tsx
+++ src/components/CartList/index.tsx
@@ -1,12 +1,15 @@
+import Button from 'components/Button';
 import GameItem, { GameItemProps } from 'components/GameItem';
+import Link from 'next/link';
 import * as S from './styles';
 
 export type CartListProps = {
   items: GameItemProps[];
   total: string;
+  hasButton?: boolean;
 };
 
-const CartList = ({ items, total }: CartListProps) => {
+const CartList = ({ items, total, hasButton = false }: CartListProps) => {
   return (
     <S.Wrapper>
       {items.map((item) => (
@@ -14,7 +17,14 @@ const CartList = ({ items, total }: CartListProps) => {
       ))}
 
       <S.Footer>
-        Total <S.Total>{total}</S.Total>
+        {!hasButton && <span>Total:</span>}
+        <S.Total>{total}</S.Total>
+
+        {hasButton && (
+          <Link href="/cart">
+            <Button as="a">Buy it now</Button>
+          </Link>
+        )}
       </S.Footer>
     </S.Wrapper>
   );
