9d6c4ff56bf0e80f55feb7bcd833a8cc9540f113
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-11-29 21:49:16 -0300

    fix(aula327): correct type on Cart template

--- src/templates/Cart/index.tsx
+++ src/templates/Cart/index.tsx
@@ -33,7 +33,7 @@ const Cart = ({
           My cart
         </Heading>
 
-        {items.length ? (
+        {items?.length ? (
           <S.Content>
             <CartList items={items} total={total} />
             <PaymentOptions cards={cards} handlePayment={handlePayment} />
