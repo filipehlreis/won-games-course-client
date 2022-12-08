af847d6607df98da6055ec6c2e5e14d3187719fa
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-12-07 21:18:02 -0300

    feat(aula344): use generated types in queries

--- src/pages/games.tsx
+++ src/pages/games.tsx
@@ -1,41 +1,18 @@
-import GamesTemplate, { GamesTemplateProps } from 'templates/Games';
-
-import filterItemsMock from 'components/ExploreSidebar/mock';
 import { initializeApollo } from 'utils/apollo';
 import { QUERY_GAMES } from 'graphql/queries/games';
+import { QueryGames, QueryGamesVariables } from 'graphql/generated/QueryGames';
+
+import GamesTemplate, { GamesTemplateProps } from 'templates/Games';
+import filterItemsMock from 'components/ExploreSidebar/mock';
 
 export default function GamesPage(props: GamesTemplateProps) {
   return <GamesTemplate {...props} />;
 }
 
-type GameProps = {
-  attributes: {
-    name: string;
-    slug: string;
-    cover: {
-      data: {
-        attributes: {
-          url: string;
-        };
-      };
-    };
-    developers: {
-      data: [
-        {
-          attributes: {
-            name: string;
-          };
-        },
-      ];
-    };
-    price: number;
-  };
-};
-
 export async function getStaticProps() {
   const apolloClient = initializeApollo();
 
-  const { data } = await apolloClient.query({
+  const { data } = await apolloClient.query<QueryGames, QueryGamesVariables>({
     query: QUERY_GAMES,
     variables: { limit: 9 },
   });
@@ -43,14 +20,16 @@ export async function getStaticProps() {
   return {
     props: {
       revalidate: 60,
-      games: data.games.data.map((game: GameProps) => ({
-        title: game.attributes.name,
-        developer: game.attributes.developers.data[0].attributes.name,
-        img: `http://localhost:1337${game.attributes.cover.data.attributes.url}`,
+      games: data.games!.data.map((game) => ({
+        title: game.attributes!.name,
+        developer: game.attributes!.developers!.data[0].attributes!.name,
+        img: `http://localhost:1337${
+          game.attributes!.cover!.data!.attributes!.url
+        }`,
         price: new Intl.NumberFormat('en', {
           style: 'currency',
           currency: 'USD',
-        }).format(game.attributes.price),
+        }).format(game.attributes!.price),
       })),
       filterItems: filterItemsMock,
     },
