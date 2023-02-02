23738950724ab9e5845d01ea2d056562a43228a7
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2023-02-01 22:01:36 -0300

    feat(aula403e404): create custom useCart hook

--- src/utils/localStorage/index.ts
+++ src/utils/localStorage/index.ts
@@ -5,7 +5,7 @@ export function getStorageItem(key: string) {
   if (typeof window === 'undefined') return;
 
   const data = window.localStorage.getItem(`${APP_KEY}_${key}`);
-  return JSON.parse(data);
+  return JSON.parse(data!);
 }
 
 export function setStorageItem(key: string, value: string[]) {
