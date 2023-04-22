6b12c2b574e462e4750c19011977d82f5ce3f481
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2023-04-22 20:53:57 -0300

    feat(aula407): add methods for total and quantity in useCart

--- src/hooks/use-cart/index.tsx
+++ src/hooks/use-cart/index.tsx
@@ -1,5 +1,6 @@
 import { useQueryGames } from 'graphql/queries/games';
 import { createContext, useContext, useEffect, useState } from 'react';
+import formatPrice from 'utils/format-price';
 import { getStorageItem } from 'utils/localStorage';
 import { cartMapper } from 'utils/mappers';
 
@@ -14,10 +15,14 @@ type CartItem = {
 
 export type CartContextData = {
   items: CartItem[];
+  quantity: number;
+  total: string;
 };
 
 export const CartContextDefaultValues = {
   items: [],
+  quantity: 0,
+  total: '$0.00',
 };
 
 export const CartContext = createContext<CartContextData>(
@@ -43,10 +48,16 @@ const CartProvider = ({ children }: CartProviderProps) => {
     variables: { filters: { id: { in: cartItems } } },
   });
 
+  const total = data?.games?.data.reduce((acc, game) => {
+    return acc + game.attributes!.price;
+  }, 0);
+
   return (
     <CartContext.Provider
       value={{
         items: cartMapper(data),
+        quantity: cartItems.length,
+        total: formatPrice(total || 0),
       }}
     >
       {children}
