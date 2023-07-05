8b72c24aa6dd1caad13f234edb5eb95a9139fea0
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2023-07-04 23:41:32 -0300

    feat(aula525): created mapper for Orders

--- src/utils/mappers/index.ts
+++ src/utils/mappers/index.ts
@@ -10,6 +10,8 @@ import {
 } from '../../graphql/generated/QueryHome';
 import { QueryWishlist_wishlists_data_attributes_games } from 'graphql/generated/QueryWishlist';
 
+import { QueryOrders_orders_data } from 'graphql/generated/QueryOrders';
+
 export const bannerMapper = (banners: QueryHome_banners) => {
   return banners!.data.map((banner) => ({
     img: `http://localhost:1337${
@@ -82,5 +84,40 @@ export const cartMapper = (games: QueryGames | undefined) => {
         title: game.attributes!.name,
         price: formatPrice(game.attributes!.price),
       }))
+    : [];
+};
+
+export const ordersMapper = (
+  orders: QueryOrders_orders_data[] | undefined | null,
+) => {
+  //
+  return orders
+    ? orders.map((order) => {
+        return {
+          id: order.id,
+          paymentInfo: {
+            flag: order.attributes?.card_brand,
+            img: order.attributes?.card_brand
+              ? `/img/cards/${order.attributes?.card_brand}.png`
+              : null,
+            number: order.attributes?.card_last4
+              ? `**** **** **** ${order.attributes?.card_last4}`
+              : 'Free Game',
+            purchaseDate: `Purchase made on ${new Intl.DateTimeFormat('en-US', {
+              day: 'numeric',
+              month: 'short',
+              year: 'numeric',
+            }).format(new Date(order.attributes?.createdAt))}`,
+          },
+          games: order.attributes?.games?.data.map((game) => ({
+            id: game.id,
+            title: game.attributes?.name,
+            downloadLink:
+              'https://wongames.com/game/download/yuYT56Tgh431LkjhNBgdf',
+            img: `http://localhost:1337${game.attributes?.cover?.data?.attributes?.url}`,
+            price: formatPrice(game.attributes!.price),
+          })),
+        };
+      })
     : [];
 };
--- src/utils/mappers/test.ts
+++ src/utils/mappers/test.ts
@@ -1,4 +1,11 @@
-import { bannerMapper, cartMapper, gamesMapper, highlightMapper } from '.';
+import { QueryOrders_orders_data } from 'graphql/generated/QueryOrders';
+import {
+  bannerMapper,
+  cartMapper,
+  gamesMapper,
+  highlightMapper,
+  ordersMapper,
+} from '.';
 import { QueryGames } from '../../graphql/generated/QueryGames';
 import { QueryGames_games } from '../../graphql/generated/QueryGames';
 import {
@@ -258,6 +265,142 @@ describe('cartMapper()', () => {
         img: 'http://localhost:1337/sample_game_2.jpg',
         title: 'Sample Game 2',
         price: '$9.90',
+      },
+    ]);
+  });
+});
+
+describe('ordersMapper()', () => {
+  it('should return empty array if no orders', () => {
+    //
+
+    expect(ordersMapper(undefined)).toStrictEqual([]);
+  });
+
+  it('should return mapped items', () => {
+    const orders = [
+      {
+        __typename: 'OrderEntity',
+        id: '1',
+        attributes: {
+          card_brand: 'visa',
+          card_last4: '4242',
+          createdAt: '2021-04-14T18:41:48.358Z',
+          games: {
+            data: [
+              {
+                id: '1',
+                attributes: {
+                  name: 'game',
+                  price: 10,
+                  developers: {
+                    data: [
+                      {
+                        attributes: {
+                          name: 'developer',
+                        },
+                      },
+                    ],
+                  },
+                  slug: 'game',
+                  cover: {
+                    data: {
+                      attributes: {
+                        url: '/image.jpg',
+                      },
+                    },
+                  },
+                },
+              },
+            ],
+          },
+        },
+      } as QueryOrders_orders_data,
+    ];
+
+    expect(ordersMapper(orders)).toStrictEqual([
+      {
+        id: '1',
+        paymentInfo: {
+          flag: 'visa',
+          img: '/img/cards/visa.png',
+          number: '**** **** **** 4242',
+          purchaseDate: 'Purchase made on Apr 14, 2021',
+        },
+        games: [
+          {
+            id: '1',
+            title: 'game',
+            downloadLink:
+              'https://wongames.com/game/download/yuYT56Tgh431LkjhNBgdf',
+            img: 'http://localhost:1337/image.jpg',
+            price: '$10.00',
+          },
+        ],
+      },
+    ]);
+  });
+
+  it('should return free game when its free', () => {
+    const orders = [
+      {
+        __typename: 'OrderEntity',
+        id: '1',
+        attributes: {
+          card_brand: null,
+          card_last4: null,
+          createdAt: '2021-04-14T18:41:48.358Z',
+          games: {
+            data: [
+              {
+                id: '1',
+                attributes: {
+                  name: 'game',
+                  price: 0,
+                  developers: {
+                    data: [
+                      {
+                        attributes: {
+                          name: 'developer',
+                        },
+                      },
+                    ],
+                  },
+                  slug: 'game',
+                  cover: {
+                    data: {
+                      attributes: {
+                        url: '/image.jpg',
+                      },
+                    },
+                  },
+                },
+              },
+            ],
+          },
+        },
+      } as QueryOrders_orders_data,
+    ];
+
+    expect(ordersMapper(orders)).toStrictEqual([
+      {
+        id: '1',
+        paymentInfo: {
+          flag: null,
+          img: null,
+          number: 'Free Game',
+          purchaseDate: 'Purchase made on Apr 14, 2021',
+        },
+        games: [
+          {
+            id: '1',
+            title: 'game',
+            downloadLink:
+              'https://wongames.com/game/download/yuYT56Tgh431LkjhNBgdf',
+            img: 'http://localhost:1337/image.jpg',
+            price: '$0.00',
+          },
+        ],
       },
     ]);
   });
