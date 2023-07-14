9e23ec64a9280e3eec627fd247f709db625b28b3
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2023-07-13 22:39:23 -0300

    feat(aula541): config next-seo to the Game page

--- src/pages/game/[slug].tsx
+++ src/pages/game/[slug].tsx
@@ -85,6 +85,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   return {
     revalidate: 60,
     props: {
+      slug: params?.slug,
       cover: `${getImageUrl(game!.cover?.data!.attributes!.src)}`,
       gameInfo: {
         id: gameId,
