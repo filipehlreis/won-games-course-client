d9205177d4b19ffe4083241bb835055503410523
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2023-07-19 23:38:44 -0300

    test(aula558): clicking and waiting for an animation with click and wait

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -2,10 +2,27 @@
 describe('Home Page', () => {
   it('should render home sections ', () => {
     //
+    cy.viewport(1000, 1000);
     cy.visit('/');
+
+    // selecionou os banners
     cy.get('.slick-slider').first().within(() => {
       cy.findByRole('heading', { name: /cyberpunk 2077/i });
       cy.findByRole('link', { name: /buy now/i });
+
+      cy.get('.slick-dots :nth-child(2) > button').click();
+      // eslint-disable-next-line cypress/no-unnecessary-waiting
+      cy.wait(500);
+
+      cy.findByRole('heading', { name: /Two Point Hospital: Bigfoot/i });
+      cy.findByRole('link', { name: /buy now/i });
+      cy.get('.slick-dots :nth-child(3) > button').click();
+      // eslint-disable-next-line cypress/no-unnecessary-waiting
+      cy.wait(500);
+
+      cy.findByRole('heading', { name: /Prey: Digital Deluxe Edition/i });
+      cy.findByRole('link', { name: /buy now/i });
+
     })
   });
 })
