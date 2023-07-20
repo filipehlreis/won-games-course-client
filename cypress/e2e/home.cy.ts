8fe9f9e33d254280af2855f63b1fb2433f41dafd
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2023-07-19 23:28:42 -0300

    test(aula557): using findByRole and create scope in cypress with within

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -3,6 +3,10 @@ describe('Home Page', () => {
   it('should render home sections ', () => {
     //
     cy.visit('/');
+    cy.get('.slick-slider').first().within(() => {
+      cy.findByRole('heading', { name: /cyberpunk 2077/i });
+      cy.findByRole('link', { name: /buy now/i });
+    })
   });
 })
 
