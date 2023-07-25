43d9f17cd961546df8b732e45f022dc2ac3e3e68
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2023-07-25 20:57:02 -0300

    test(aula580): create fake data with JS factory bot

---
+++ cypress/e2e/user.cy.ts
@@ -0,0 +1,23 @@
+import { createUser } from "../support/generate";
+
+describe('User', () => {
+  beforeEach(() => {
+    cy.viewport(1000, 1000);
+  })
+
+  it('should sign up', () => {
+    const user = createUser()
+
+    cy.visit('/sign-up')
+
+    cy.findByPlaceholderText(/username/i).type(user.username)
+    cy.findByPlaceholderText(/email/i).type(user.email)
+    cy.findByPlaceholderText(/^password/i).type(user.password)
+    cy.findByPlaceholderText(/confirm password/i).type(user.password)
+
+    cy.findByRole('button', { name: /sign up now/i }).click()
+
+    cy.url().should('eq', `${Cypress.config().baseUrl}/`)
+    cy.findByText(user.username).should('exist')
+  })
+})
